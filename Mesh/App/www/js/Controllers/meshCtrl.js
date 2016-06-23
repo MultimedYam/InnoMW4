71c03e9559d819c2d214db4e4c410d82703514c1
Robert Roubos <robertroubos9@gmail.com>
2016-06-23 10:54:28 +0200

    no message

--- Mesh/App/www/js/Controllers/meshCtrl.js
+++ Mesh/App/www/js/Controllers/meshCtrl.js
@@ -1,10 +1,56 @@
+myApp.controller('meshCtrl', function ($scope, $ionicSideMenuDelegate, websocket) {
 
-myApp.controller('meshCtrl', function ($scope, $ionicSideMenuDelegate) {
+    var socket = new WebSocket(websocket);
+    console.log(socket); 
+
+    socket.onopen = function(){
+    socket.send('{"request":"getMyInterests"}');
+    socket.onmessage = onMessage;
+
+};
+
+    function onMessage(event) {
+        console.log("onmessage");
+
+        console.log(event);
+        var data = JSON.parse(event.data);
+        console.log(data);
+        // var JSONList = data["myInterests"];
+    }
 
 
-    $scope.toggleLeft = function() {
+    // for (var prop in JSONList) {
+    //     console.log("Interest " + prop + " / Category " + JSONList[prop]);
+    // }
+
+    //for each interest in interests
+    $scope.interests = {
+        hex1: {
+            category: 'balance',
+            interest: 'balance'
+        }, hex2: {
+            category: 'photography',
+            interest: 'photography'
+        }, hex3: {
+            category: 'writing',
+            interest: 'writing'
+        },  hex5: {
+            category: 'balance',
+            interest: 'balance'
+        }, hex6: {
+            category: 'balance',
+            interest: 'balance'
+        }, hex7: {
+            category: 'balance',
+            interest: 'balance'
+        }
+    };
+
+    $scope.toggleLeft = function () {
         $ionicSideMenuDelegate.toggleLeft();
     };
 
-
+    angular.element(document).ready(function () {
+        console.log("test");
+    });
 });//controller
